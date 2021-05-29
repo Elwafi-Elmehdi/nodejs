23c3e5af2ce07f677e1ff24a232fbadef7d04c18
Mehdi <elwafi.courrier99@gmail.com>
2021-05-30 00:43:28 +0100

    Advanced Templating i web-server app

--- web-server/source/app.js
+++ web-server/source/app.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
+
 const app = express()
 
 // Paths
 const publicDir = path.join(__dirname,'../public')
-const viewsDir = path.join(__dirname,'../templates')
+const viewsDir = path.join(__dirname,'../templates/views')
+const partialsDir = path.join(__dirname,'../templates/partials')
 
+// Partials
+hbs.registerPartials(partialsDir)
 
 // Serving Static dir
 app.use(express.static(publicDir))
@@ -15,15 +21,26 @@ app.set('view engine','hbs')
 app.set('views',viewsDir)
 
 app.get('',(req,res)=>{
- res.render('index')
+ res.render('index',{
+  title:'Weather',
+  name:'Mehdi',
+})
 })
 
 app.get('/about',(req,res)=>{
- res.render('about',{msg:'ABout page created by Mehdi Elwafi'})
+ res.render('about',{
+  title:'About',
+  name:'Mehdi',
+  msg:'About page created by Mehdi Elwafi'
+ })
 })
 
 app.get('/help',(req,res)=>{
- res.render('help',{desc:'You can contact @mehdi for help'})
+ res.render('help',{
+  title:'Help',
+  name:'Mehdi',
+  desc:'You can contact @mehdi for help'
+ })
 })
 
 app.listen(3000,() => {
