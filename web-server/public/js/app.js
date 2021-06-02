c0f724c25e7abeefb574ed1bd52ab90b5b4e7b44
Mehdi <elwafi.courrier99@gmail.com>
2021-06-02 14:52:05 +0100

    Completed Weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,14 +1,22 @@
 let weatherForm = document.querySelector('#weatherForm')
 let loc = document.querySelector('#location')
 
+let messageOne = document.querySelector('#msgOne')
+let messageTwo = document.querySelector('#msgTwo')
+
+
+
 weatherForm.addEventListener('submit',(e) => {
  e.preventDefault()
-
+ messageOne.textContent = 'Loading...'
  fetch('http://localhost:3000/weather?address='+encodeURIComponent(loc.value)).then((response)=>{
   response.json().then((data)=>{
    if(data.error)
-    return console.log(data)
-   console.log(data);
+    return messageOne.textContent = data.error
+
+    let {location='Monzanbi9',forecast='Walo'} = data
+    messageOne.textContent = location.desc
+    messageTwo.textContent = `${forecast.weather_descriptions[0]}, its ${forecast.temperature}C and it feels like  ${forecast.feelslike}C, and There is ${forecast.precip}% of raining`
   })
  })
 })
