3d0bacccaaaca58bf79afa80dae0ae640e93cf84
Mehdi <elwafi.courrier99@gmail.com>
2021-06-23 22:12:19 +0100

    Added Delete Avatar image

--- task-manager/src/provided/userProvided.js
+++ task-manager/src/provided/userProvided.js
@@ -166,4 +166,14 @@ router.post('/users/me/avatar',auth,uplaod.single('avatar'), async (req,res) => 
   res.status(400).send({error:error.message})
 })
 
+router.delete('/users/me/avatar',auth,async (req,res)=>{
+  try {
+    req.user.avatar = undefined;
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
 module.exports = router
