30050c5c98b552dda2cd7ce06ba32570720c698c
Mehdi <elwafi.courrier99@gmail.com>
2021-06-24 22:57:37 +0100

    init new express project and added Security Consts

---
+++ notes-app-express/src/consts/security.js
@@ -0,0 +1,10 @@
+const securityConsts = {
+    signature:"\\At=*\\c)GXeH8q2p_E*J$a-K>a[^8~",
+    tokenHeader:"Jwt-Token",
+    tokenExpiration: "5 days",
+    unauthorized:"You are unauthorized please login to access to page",
+    internalServerError:"Internal Server Error",
+    pageNotFound:"Page not found",
+    badCredentials:"email/passowrd are incorrect please try again",
+    emailTaken:"Email already taken,please try with a diffrent email"
+}
