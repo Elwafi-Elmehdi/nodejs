530bdd75a27bac16fef135554bcdea2789dea103
Mehdi <elwafi.courrier99@gmail.com>
2021-06-25 01:11:38 +0100

    Added User Created

--- notes-app-express/src/routes/User.js
+++ notes-app-express/src/routes/User.js
@@ -3,14 +3,18 @@ const router = new express.Router()
 const User = require('../models/User')
 const resConsts = require('../consts/responce')
 const securityConsts = require('../consts/security')
+const url = '/users'
 
-router.get('/users',async (req,res)=>{
+router.get(url,async (req,res)=>{
     try {
         const users = await User.find({})
         res.send(users)
     }catch (e){
         res.status(500).send({error: resConsts.internalServerError})
     }
+})
+router.post(url,async (req,res)=>{
+    res.send('res')
 })
 
 module.exports = router
