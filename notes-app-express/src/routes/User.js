7e4ebb630176d29b0e2feb14dbad4342d9a34ed1
Mehdi <elwafi.courrier99@gmail.com>
2021-06-25 01:02:18 +0100

    updated Read Users EndPoint

--- notes-app-express/src/routes/User.js
+++ notes-app-express/src/routes/User.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = new express.Router()
+const User = require('../models/User')
 const resConsts = require('../consts/responce')
 const securityConsts = require('../consts/security')
 
 router.get('/users',async (req,res)=>{
-    res.send("Users Route is up")
+    try {
+        const users = await User.find({})
+        res.send(users)
+    }catch (e){
+        res.status(500).send({error: resConsts.internalServerError})
+    }
 })
 
 module.exports = router
