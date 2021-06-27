1ddc2d2e7e37eecfa82683e93186a161fb318a82
Mehdi <elwafi.courrier99@gmail.com>
2021-06-27 22:41:27 +0100

    Updated auth middleware

--- notes-app-express/src/middleware/auth.js
+++ notes-app-express/src/middleware/auth.js
@@ -1,1 +1,21 @@
 const jwt = require('jsonwebtoken')
+const consts = require('../consts/security')
+const resConsts = require('../consts/responce')
+const User = require('../models/User')
+
+
+
+const auth = async (req,res, next) => {
+    try {
+        const token = req.get(consts.tokenHeader).substr('Bearer '.length)
+        if(!token){
+            res.status(401).send({error:resConsts.unauthorized})
+        }
+        const payload = await jwt.verify(token,consts.signature)
+        const user = await User.findById(payload._id)
+        req.user = user
+        next()
+    }catch (e) {
+        res.status(401).send({error:resConsts.unauthorized})
+    }
+}
