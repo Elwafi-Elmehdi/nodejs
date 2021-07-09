fc42a900464772b42a463da6be669ac1fd13e1df
Mehdi <elwafi.courrier99@gmail.com>
2021-07-09 08:28:10 +0100

    Updated task Model

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 // const validator = require('validator')ss
 
 const TaskSchema = new mongoose.Schema({
+ label:{
+  type:String,
+  required:true,
+  trim:true
+ },
  desc:{
   trim:true,
   required:true,
