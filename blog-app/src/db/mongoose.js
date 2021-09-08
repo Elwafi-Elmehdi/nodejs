f4abe85c759b50b580135c70841ddd79e443e347
Mehdi <elwafi.courrier99@gmail.com>
2021-09-08 21:34:05 +0100

    Added config mongoose

---
+++ blog-app/src/db/mongoose.js
@@ -0,0 +1,9 @@
+const db = require('mongoose')
+
+const dbName = "mongodb://127.0.0.1:27017/blog-api"
+
+db.connect(dbName,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
