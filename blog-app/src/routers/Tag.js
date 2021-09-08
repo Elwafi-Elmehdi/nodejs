be3a7dd0c832ad1dc7adb3f71bfe724425c228da
Mehdi <elwafi.courrier99@gmail.com>
2021-09-09 00:43:38 +0100

    Added router for tag

---
+++ blog-app/src/routers/Tag.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const Category = require('../models/Category')
+const router = new express.Router();
+
+router.post('/categories/',async (req,res)=>{
+    try {
+        const category = new Category({...req.body});
+        await category.save();
+        res.send(category)
+    }
+    catch (e) {
+        res.status(400).send({error:"User Error"})
+    }
+
+})
+
+module.exports = router
