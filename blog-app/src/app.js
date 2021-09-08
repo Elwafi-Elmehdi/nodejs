3b419c7528856b2bcb637adb572c5c8288f2e18c
Mehdi <elwafi.courrier99@gmail.com>
2021-09-08 21:38:00 +0100

    Added Category model

--- blog-app/src/app.js
+++ blog-app/src/app.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const app = express();
+require('./db/mongoose')
 
 app.use(express.json)
 
 const PORT = process.env.PORT || 3000
-
 
 app.listen(() => {
     console.log("Listening on ",PORT)
