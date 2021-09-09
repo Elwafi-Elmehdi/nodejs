cec3691c8a39b4ef3f296c5e4e057510bfabe2a4
Mehdi <elwafi.courrier99@gmail.com>
2021-09-09 19:28:00 +0100

    Added async test case

--- task-manager/tests/add.test.js
+++ task-manager/tests/add.test.js
@@ -3,3 +3,11 @@ const add = (x, y) => x + y;
 test("Testing add function",()=>{
     expect(add(1,1)).toBe(2)
 })
+
+
+test("should throw error async ",(done)=>{
+    setTimeout(()=>{
+        expect(1).toBe(2);
+        done()
+    },2000)
+})
