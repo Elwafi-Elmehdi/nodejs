8cd4493d452c59d165962b4e2677624565c94f20
Mehdi <elwafi.courrier99@gmail.com>
2021-09-09 23:35:31 +0100

    Added jest lifecycle beforeeach

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+const user1 = {
+    firstname:"Reda",
+    lastname:"hihi",
+    email:"hehe@ewhhew.hehe",
+    password:"hehehe1777!",
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(user1).save()
+})
 
 test('Should register a new user',async () => {
     await request(app).post('/users/register').send({
