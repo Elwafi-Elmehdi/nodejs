11fa03bd52019d1f956fd9d1e1710b6b07c9a4cc
Mehdi <elwafi.courrier99@gmail.com>
2021-09-10 19:41:27 +0100

    Added test case for read profile end point

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -4,11 +4,16 @@ const User = require('../src/models/user')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 
+const user1Id = new mongoose.Types.ObjectId();
 const user1 = {
+    _id:user1Id,
     firstname:"Reda",
     lastname:"hihi",
     email:"hehe@ewhhew.hehe",
     password:"hehehe1777!",
+    tokens:[{
+        token:jwt.sign({_id:user1Id},process.env.JWT_TOKEN)
+    }]
 }
 
 beforeEach(async () => {
@@ -37,4 +42,12 @@ test('Should not login unkown user',async () => {
         email: user1.email,
         password: "passsqw7784@"
     }).expect(404)
+})
+
+test("Should get user profile",async () =>{
+    await request(app)
+        .get('/users/me')
+        .set('Authorization',`Bearer ${user1.tokens[0].token}`)
+        .send()
+        .expect(200)
 })
