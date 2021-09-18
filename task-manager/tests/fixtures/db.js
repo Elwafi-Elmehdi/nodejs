bf149a4df49de4db87c1114299efb33d3e262bd3
Mehdi <elwafi.courrier99@gmail.com>
2021-09-18 16:45:04 +0100

    Updated db fixure

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const User = require("../../src/models/user");
+const Task = require("../../src/models/task");
 
 const user1Id = new mongoose.Types.ObjectId();
 const user1 = {
@@ -16,9 +17,27 @@ const user1 = {
 	],
 };
 
+const user2Id = new mongoose.Types.ObjectId();
+const user2 = {
+	_id: user1Id,
+	firstname: "Mehdi",
+	lastname: "hihi",
+	email: "mehdi@ewhhew.hehe",
+	password: "washasat#14!",
+	tokens: [
+		{
+			token: jwt.sign({ _id: user2Id }, process.env.JWT_TOKEN),
+		},
+	],
+};
+
+
+
 const setUpDB = async () => {
 	await User.deleteMany();
+	await Task.deleteMany();
 	await new User(user1).save();
+	await new User(user2).save();
 };
 
 module.exports = {
