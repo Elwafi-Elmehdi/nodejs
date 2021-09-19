90e521011da0fdcffe39c6e71d70717109db2daf
Mehdi <elwafi.courrier99@gmail.com>
2021-09-19 16:19:57 +0100

    UPdated db fixture

--- notes-app-express/tests/fixtures/db.js
+++ notes-app-express/tests/fixtures/db.js
@@ -1,1 +1,24 @@
-require('../../src/db/mongoose')
+const mongoose = require('mongoose')
+const User = require('../../src/models/User')
+const jwt = require('jsonwebtoken')
+
+const userOneId = new mongoose.Types._ObjectId()
+
+const userOne = {
+    _id: userOneId,
+    firstname:"Mehdi",
+    lastname:"ELwafi",
+    email:"mehdi@mehdi.dev",
+    password:"Hello,Wolrd@#"
+}
+
+const initDB = async () => {
+    await User.deleteMany()
+    new User(userOne).save()
+}
+
+module.exports = {
+    userOneId,
+    userOne,
+    initDB
+}
