2d8d8dfcfcffa1be769d81e1c485acbd6c2fc983
Mehdi <elwafi.courrier99@gmail.com>
2021-09-19 20:31:49 +0100

    Added test case for login user note app

--- notes-app-express/tests/user.test.js
+++ notes-app-express/tests/user.test.js
@@ -1,10 +1,24 @@
-const User = require('../src/models/User')
-const app = require('../src/app')
-const {userOne,userOneId,initDB} = require('./fixtures/db')
+const request = require("supertest");
+const User = require("../src/models/User");
+const app = require("../src/app");
+const { userOne, userOneId, initDB } = require("./fixtures/db");
 
-beforeEach(initDB)
+beforeEach(initDB);
 
-test("Should setup userOne in db",async () => {
-    const user = await User.findById(userOneId)
-    expect(user._id).toMatchObject(userOneId)
+test("Should setup userOne in db", async () => {
+	const user = await User.findById(userOneId);
+	expect(user._id).toMatchObject(userOneId);
+});
+
+test("Should not login to the count", async () => {
+	const response = await request(app)
+		.post("/login")
+		.send({
+			email: userOne.email,
+			passowd: userOne.password,
+		})
+		.expect(200);
+	expect(response.body).toMatchObject({
+		...userOne,
+	});
 });
