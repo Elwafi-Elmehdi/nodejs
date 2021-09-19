cb3fc699b4959175ae5dc9d9323d0e67e1af0387
Mehdi <elwafi.courrier99@gmail.com>
2021-09-19 16:32:51 +0100

    Updated user test suite

--- notes-app-express/tests/user.test.js
+++ notes-app-express/tests/user.test.js
@@ -1,4 +1,10 @@
+const User = require('../src/models/User')
+const app = require('../src/app')
 const {userOne,userOneId,initDB} = require('./fixtures/db')
 
 beforeEach(initDB)
 
+test("Should setup userOne in db",async () => {
+    const user = await User.findById(userOneId)
+    expect(user).not.toBeNull()
+});
