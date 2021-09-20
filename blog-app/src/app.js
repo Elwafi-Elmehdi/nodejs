c2d0cc1ae816b4e763d8992ef790641b48af378f
Mehdi <elwafi.courrier99@gmail.com>
2021-09-20 17:38:07 +0100

    Added post router to app

--- blog-app/src/app.js
+++ blog-app/src/app.js
@@ -1,15 +1,17 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-require('./db/mongoose')
+require("./db/mongoose");
 
-const tagRouter = require('./routers/tag')
-const userRouter = require('./routers/user')
-const categoryRouter = require('./routers/category')
+const tagRouter = require("./routers/tag");
+const userRouter = require("./routers/user");
+const categoryRouter = require("./routers/category");
+const postRouter = require("./routers/post");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(userRouter)
-app.use(categoryRouter)
-app.use(tagRouter)
+app.use(userRouter);
+app.use(categoryRouter);
+app.use(tagRouter);
+app.use(postRouter);
 
-module.exports = app
+module.exports = app;
--- blog-app/src/routers/post.js
+++ blog-app/src/routers/post.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = new express.Router();
+const Post = require("../models/post");
 
-
-
+router.get("/posts/", async (req, res) => {
+	res.send("Hell from post router");
+});
 
 module.exports = router;
