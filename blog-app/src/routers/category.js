bf726fa7eac0d86e5e940ec7d87d21ac73659106
Mehdi <elwafi.courrier99@gmail.com>
2021-09-20 15:34:54 +0100

    Updated notte-express

--- blog-app/src/routers/category.js
+++ blog-app/src/routers/category.js
@@ -1,45 +1,44 @@
-const express = require('express');
-const Category = require('../models/category')
+const express = require("express");
+const Category = require("../models/category");
 const router = new express.Router();
 
-router.post('/categories',async (req,res)=>{
-    try {
-        const category = new Category({...req.body});
-        await category.save();
-        res.send(category)
-    }
-    catch (e) {
-        res.status(400).send({error:"User Error"})
-    }
-})
+router.post("/categories", async (req, res) => {
+	try {
+		const category = new Category({ ...req.body });
+		await category.save();
+		res.send(category);
+	} catch (e) {
+		res.status(400).send({ error: "User Error" });
+	}
+});
 
-router.get('/categories/all',async (req,res)=>{
-    try {
-        const categories = await Category.find({})
-        res.send(categories)
-    }catch (e) {
-        res.status(500).send()
-    }
-})
+router.get("/categories/all", async (req, res) => {
+	try {
+		const categories = await Category.find({});
+		res.send(categories);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
-router.get('/categories/:title',async (req,res)=>{
-    try {
-        const title = req.params.title
-        const category = await Category.findOne({title:title})
-        res.send(category)
-    }catch (e) {
-        res.status(500).send()
-    }
-})
+router.get("/categories/:title", async (req, res) => {
+	try {
+		const title = req.params.title;
+		const category = await Category.findOne({ title: title });
+		res.send(category);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
-router.delete('/categories/:title',async (req,res)=>{
-    try {
-        const title = req.params.title
-        const category = await Category.findOneAndDelete({title:title})
-        res.send(category)
-    }catch (e) {
-        res.status(500).send()
-    }
-})
+router.delete("/categories/:title", async (req, res) => {
+	try {
+		const title = req.params.title;
+		const category = await Category.findOneAndDelete({ title: title });
+		res.send(category);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
 
-module.exports = router
+module.exports = router;
