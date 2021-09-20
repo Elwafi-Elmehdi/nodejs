4ef2cefaf6b603eb3f3b49b09a42853f00934536
Mehdi <elwafi.courrier99@gmail.com>
2021-09-20 17:55:17 +0100

    updatedpost model and jwt

--- blog-app/src/models/post.js
+++ blog-app/src/models/post.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
+const postSchema = new mongoose.Schema(
+	{
+		summary: {
+			type: String,
+		},
+		title: {
+			type: String,
+			unique: true,
+		},
+		content: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
 
-const postSchema = new mongoose.Schema({
-    summary:{
-        type:String
-    },
-    title:{
-        type:String
-    },
-    content:{
-        type:String
-    }
-},{timestamps:true})
+const Post = new mongoose.model("Post", postSchema);
 
-const Post = new mongoose.model('Post',postSchema)
-
-module.exports = Post
+module.exports = Post;
--- blog-app/src/models/user.js
+++ blog-app/src/models/user.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    fullname: {
-        type:String,
-        required:true,
-    },
-    age: {
-        type:Number,
-        required:true,
-        validate(age) {
-            if(age <= 0){
-                throw new Error("Age is not valid.")
-            }
-        }
-    },
-    email:{
-        type:String,
-        unique:true,
-        required:true,
-    },
-    password:{
-        type:String
-    }
-})
+const userSchema = new mongoose.Schema(
+	{
+		fullname: {
+			type: String,
+			required: true,
+		},
+		age: {
+			type: Number,
+			required: true,
+			validate(age) {
+				if (age <= 0) {
+					throw new Error("Age is not valid.");
+				}
+			},
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		password: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
 
-const User = new mongoose.model('User',userSchema);
+const User = new mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
