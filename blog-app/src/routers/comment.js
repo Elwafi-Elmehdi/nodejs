810faf1c63ed122e30f7497397be5e85152b8c26
Mehdi <elwafi.courrier99@gmail.com>
2021-09-22 00:21:16 +0100

    Added create comment

--- blog-app/src/routers/comment.js
+++ blog-app/src/routers/comment.js
@@ -5,8 +5,22 @@ const Comment = require("../models/comment");
 router.get("/comments/post/:id", async (req, res) => {
 	try {
 		const id = req.params.id;
+		if (!id) {
+			return res.status(400).send();
+		}
 		const comments = await Comment.find({ post: id });
 		res.send(comments);
+	} catch (error) {
+		res.status(500).send();
+	}
+});
+
+router.post("/comments/post/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		if (!id) {
+			return res.status(400).send();
+		}
 	} catch (error) {
 		res.status(500).send();
 	}
