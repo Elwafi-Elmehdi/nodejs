92dfb22c0e02245adbc5dc954084041851bbff5a
Mehdi <elwafi.courrier99@gmail.com>
2021-09-22 00:18:31 +0100

    Added end-point for comments by post

--- blog-app/src/routers/comment.js
+++ blog-app/src/routers/comment.js
@@ -2,5 +2,14 @@ const router = new require("express").Router();
 const auth = require("../middleware/auth");
 const Comment = require("../models/comment");
 
+router.get("/comments/post/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const comments = await Comment.find({ post: id });
+		res.send(comments);
+	} catch (error) {
+		res.status(500).send();
+	}
+});
 
 module.exports = router;
