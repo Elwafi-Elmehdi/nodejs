ab616fcaa4c34118f0bf6d38a18b454cae2488cc
Mehdi <elwafi.courrier99@gmail.com>
2021-09-21 01:15:17 +0100

    Updated comment model

--- blog-app/src/models/comment.js
+++ blog-app/src/models/comment.js
@@ -1,3 +1,18 @@
 const { Schema, model } = require("mongoose");
 
-const commentSchema = new Schema({}, { timestamps: true });
+const commentSchema = new Schema(
+	{
+		title: {
+			type: String,
+		},
+		content: {
+			required: true,
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Comment = model("Comment", commentSchema);
+
+module.exports = Comment;
