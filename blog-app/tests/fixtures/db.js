72beae993a00928a2df47c35dbfd93151672ec9a
Mehdi <elwafi.courrier99@gmail.com>
2021-09-22 21:46:08 +0100

    Updated db fixture added data

--- blog-app/tests/fixtures/db.js
+++ blog-app/tests/fixtures/db.js
@@ -9,3 +9,45 @@ const Tag = require("../../src/models/tag");
 
 const userOneId = new ObjectId();
 
+const userOne = {
+	_id: userOneId,
+	age: 22,
+	email: "test@mehdi.dev",
+	password: "Tests$@&",
+	fullname: "Blog Tests",
+};
+
+const tagOne = {
+	title: "HTML",
+	content: "HTML 5",
+};
+
+const tagTow = {
+	title: "CSS",
+	content: "CSS4",
+};
+
+const categoryOne = {
+	title: "features",
+	content: "",
+};
+
+const categoryTwo = {
+	title: "bugs",
+	content: "",
+};
+
+const initDB = async () => {
+	await User.deleteMany({});
+	await Post.deleteMany({});
+	await Comment.deleteMany({});
+	await Tag.deleteMany({});
+	await Category.deleteMany({});
+
+	await User.save(userOne);
+	await Category.save(categoryOne);
+	await Category.save(categoryTwo);
+
+	await Tag.save(tagOne);
+	await Tag.save(tagTow);
+};
