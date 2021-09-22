1b1568d104a2c386432abc115b199a66eaecb074
Mehdi <elwafi.courrier99@gmail.com>
2021-09-22 21:25:24 +0100

    Updated db fixture

---
+++ blog-app/tests/fixtures/db.js
@@ -0,0 +1,11 @@
+const { ObjectId } = require("mongoose");
+
+// Models
+const Post = require("../../src/models/post");
+const User = require("../../src/models/user");
+const Category = require("../../src/models/category");
+const Comment = require("../../src/models/comment");
+const Tag = require("../../src/models/tag");
+
+const userOneId = new ObjectId();
+
