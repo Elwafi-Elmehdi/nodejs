4ddca20bd912b9b981120b5e4f36e2f633e7e43d
Mehdi <elwafi.courrier99@gmail.com>
2021-09-25 22:12:37 +0100

    Added test case for init tags in db

--- blog-app/tests/tags.test.js
+++ blog-app/tests/tags.test.js
@@ -1,6 +1,14 @@
-const {initDB} = require('./fixtures/db')
+const {initDB,tags} = require('./fixtures/db')
 const request = require('supertest')
 const app = require('../src/app')
 
 beforeEach(initDB)
+
+test("Should get all the tags",async () => {
+    const response = await request(app)
+        .get('/tags/all')
+        .send()
+        .expect(200)
+    expect(response.body).toHaveLength(2)
+})
 
