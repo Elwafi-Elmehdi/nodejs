913394f9de15418697f2befa123705003d4c34e8
Mehdi <elwafi.courrier99@gmail.com>
2021-10-05 23:38:28 +0100

    Fixed test errors in blog app

--- blog-app/tests/categories.test.js
+++ blog-app/tests/categories.test.js
@@ -1,16 +1,16 @@
-const { initDB,categories } = require("./fixtures/db");
+const { initDB, categories } = require("./fixtures/db");
 const { Category } = require("../src/models/category");
-const request = require('supertest')
-const app = require('../src/app')
+const request = require("supertest");
+const app = require("../src/app");
 
 beforeEach(initDB);
 
 test("Should get all categories", async () => {
-    const response = await request(app)
-        .get('/categories/all')
-        .send()
-        .expect(200)
-    expect(response.body).toHaveLength(2)
+	const response = await request(app)
+		.get("/categories/all")
+		.send()
+		.expect(200);
+	expect(response.body).toHaveLength(2);
 });
 
-test("Should not delete any category")
+// test("Should not delete any category");
--- blog-app/tests/tags.test.js
+++ blog-app/tests/tags.test.js
@@ -1,26 +1,18 @@
-const {initDB,tags} = require('./fixtures/db')
-const request = require('supertest')
-const app = require('../src/app')
+const { initDB, tags } = require("./fixtures/db");
+const request = require("supertest");
+const app = require("../src/app");
 
-beforeEach(initDB)
+beforeEach(initDB);
 
-test("Should get all the tags",async () => {
-    const response = await request(app)
-        .get('/tags/all')
-        .send()
-        .expect(200)
-    expect(response.body.length).toBe(2)
-})
+test("Should get all the tags", async () => {
+	const response = await request(app).get("/tags/all").send().expect(200);
+	console.error(response.body);
+	expect(response.body).toEqual(tags);
+});
 
-test("Should get tag with title",async () => {
-    const response = await request(app)
-        .get('/tags/CSS')
-        .send()
-        .expect(200);
-    console.error(response.body)
-    expect(response.body).toMatchObject({
-        title:tags[1].title
-    })
-})
-
-
+test("Should get tag with title", async () => {
+	const response = await request(app).get("/tags/CSS").send().expect(200);
+	expect(response.body).toMatchObject({
+		title: tags[1].title,
+	});
+});
