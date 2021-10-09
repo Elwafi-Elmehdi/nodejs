b90ac617e9d1c9cf9042567621e414e8589eb47d
Mehdi <elwafi.courrier99@gmail.com>
2021-10-09 22:23:17 +0100

    Added product model

--- sequlize-app/index.js
+++ sequlize-app/index.js
@@ -1,7 +1,9 @@
 const app = require("./src/app");
+const { sequelize } = require("./models/index");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+	await sequelize.sync();
 	console.log("Server running on ", PORT);
 });
