7d676a3bb8b5096edc59274a24d922042f92e09e
Mehdi <elwafi.courrier99@gmail.com>
2021-10-09 23:23:02 +0100

    Updated the product router added findby Ref

--- sequlize-app/src/routes/product.js
+++ sequlize-app/src/routes/product.js
@@ -27,4 +27,21 @@ router.get("/products", async (req, res) => {
 	}
 });
 
+router.get("/products/:ref", async (req, res) => {
+	try {
+		const ref = req.params.ref;
+		if (!ref) {
+			throw new Error("Client Input Error");
+		}
+		const producta = await product.findAll({
+			where: {
+				ref,
+			},
+		});
+		res.send(producta);
+	} catch (error) {
+		res.status(500).send({ error: error.message });
+	}
+});
+
 module.exports = router;
