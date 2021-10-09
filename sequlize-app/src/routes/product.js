9816fe24f39a84e82a0b376f621884eeb1ea050a
Mehdi <elwafi.courrier99@gmail.com>
2021-10-09 22:42:20 +0100

    Added create product

--- sequlize-app/src/routes/product.js
+++ sequlize-app/src/routes/product.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const {} = require('')
+const { sequelize, Product } = require("../../models/");
 const router = new express.Router();
 
 router.get("/products", async (req, res) => {
-
+	try {
+		const { ref, category } = req.body;
+		if (!ref || !category) {
+			throw new Error("Client Input Error");
+		}
+		const product = await Product.create({
+			ref,
+			category,
+		});
+        
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
